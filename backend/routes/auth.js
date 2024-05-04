59af8c8f1aeabc70f4ac68f53b7638c8c366b620
Avanish shukla <36558094+avishukl96@users.noreply.github.com>
2024-05-05 00:16:17 +0530

    Validation, Name, Email, Password , Error Handling , showing errors etc

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,38 @@
 const express = require("express");
-
+const { body, validationResult } = require("express-validator");
 const User = require("../models/Users");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+router.get(
+  "/",
+  [
+    body("name", "Enter a valid Name").isLength({ min: 3 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password must be atleast 5 charectors").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    //Check validation
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ erros: errors.array() });
+    }
+    //Adding Database
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        res.json({
+          error: "Email id is already in used, Please Enter unique Email ",
+          message: err.message,
+        });
+      });
+  }
+);
 
 module.exports = router;
